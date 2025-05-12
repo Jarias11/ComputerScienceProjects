f0c4ccca2d6058d913cb92823c89f9a3e766b960
Jeremy Arias <jeremyraph@msn.com>
2025-05-12 00:52:44 -0400

    Add hello world intro program

---
+++ 01-Intro/main (1).cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Jeremy Arias
+CS 280
+1/25/2021
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Welcome to CS 280."<<endl;
+    cout<<"What is your name?"<<endl;
+    string name;
+    cin>>name;
+    cout<<"Hello, "<<name<<endl;
+    return 0;
+}
